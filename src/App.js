38c24afa8eb113d153fdbac03ed878158662620d
Gabrielle Brumer <gabriellebrumer@hotmail.com>
2022-08-25 20:54:55 -0300

    Api de busca de CEP em andamento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
+import api from './services/api';
 
 function App() {
+  const [input, setInput] = useState('')
+
+  async function handleSearch(){
+    if(input === ''){
+      alert ("Prencher o campo de CEP")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      console.log(response.data)
+    } catch {
+      alert("Ops! Erro ao buscar CEP")
+      setInput('')
+    }
+  }
   return (
     <div className="container">
       <h1 className="title">BUSCADOR DE CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP" />
-        <button className="buttonSearch">
+        <input type="text" placeholder="" value={input} onChange={(e) => setInput(e.target.value)}/>
+
+        <button className="buttonSearch" onClick={handleSearch}>
           < FiSearch size={25} color="#FFF" />
         </button>
       </div>
